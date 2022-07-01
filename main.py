a9fd4d82ce48ef1fe95bd397c73eb2729ac3ac6b
Michele Acocella <michelemitchacocella@gmail.com>
2022-07-01 18:36:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import telegram
 async def main():
     bot = telegram.Bot("5517966699:AAHgqcf3-oxrnYCEBYRsR6wl9SDUehjCFsU")
     async with bot:
-        await bot.send_message(text='Hi John!', chat_id=27957501)
+        await bot.send_message(text='Hi John!', chat_id=-641379699)
 
 
 if __name__ == '__main__':
